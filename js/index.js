5166cbf18a68c2cc01fc72ea596c14232c1774b5
Rikelvem Silva <rikelvemrsilva@gmail.com>
2022-01-06 23:59:30 -0300

    menu responsivo BOLADO

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const lista = document.querySelectorAll('.lista');
+function ativoLink(){
+    lista.forEach((item) =>
+    item.classList.remove('ativo'));
+    this.classList.add('ativo');
+}
+lista.forEach((item) =>
+item.addEventListener('click', ativoLink))
