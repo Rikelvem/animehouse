ec19517146375402df32a8565a87a317a67bfa85
Rikelvem Silva <rikelvemrsilva@gmail.com>
2022-01-08 22:23:46 -0300

    andamento do projeto

--- js/index.js
+++ js/index.js
@@ -9,6 +9,42 @@ function ativoLink(){
 lista.forEach((item) =>
 item.addEventListener('click', ativoLink))
 
+const debounce = function(func, wait, immediate) {
+    let timeout;
+    return function(...args) {
+    const context = this;
+    const later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+    };
+};
+
+const cabecalho = document.querySelectorAll('.cabecalho');
+const cabecalhoClass = 'mostrar__ativo';
+
+function cabecalhoScroll() {
+    const windowTop = window.pageYOffset;
+    cabecalho.forEach(function(element) {
+        if(windowTop > 20) {
+            element.classList.add(cabecalhoClass);
+        }
+        if(windowTop < 20) {
+            element.classList.remove(cabecalhoClass);
+        }
+    })
+}
+
+if(cabecalho.length) {
+window.addEventListener('scroll', debounce(function() {
+    cabecalhoScroll();
+}, 200));
+}
+
 // TOGGLE DARKMODE
 
 const body = document.querySelector('body');
@@ -17,3 +53,26 @@ toggle.onclick = function(){
     toggle.classList.toggle('darkmode__ativo');
     body.classList.toggle('darkmode__ativo')
 }
+
+// MODAL BOLADO
+
+function iniciaModal(modalID){
+    if(localStorage.fechamodal !== modalID){
+        const modal = document.getElementById(modalID);
+        modal.classList.add('mostrar')
+        if(modal){
+            modal.addEventListener('click', (e) => {
+                if(e.target.id == modalID || e.target.className == 'modal__fechar') {
+                    modal.classList.remove('mostrar');
+                    localStorage.fechamodal = modalID;
+                }
+            })
+        }
+    }
+}
+
+document.addEventListener('scroll', () => {
+    if(window.pageYOffset > 100){
+        iniciaModal('modal__mensagem');
+    }
+})
